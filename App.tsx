b7336c52af8209162a8b6365499cfeb967fb411d
João Gabriel <joao@3035tech.com>
2023-06-29 15:37:20 -0300

    feat: add miminal repro

--- App.tsx
+++ App.tsx
@@ -1,11 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { RefObject, useEffect, useRef } from "react";
+import {
+  Dimensions,
+  FlatList,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
+const { width } = Dimensions.get("window");
+
+const ITEM_SIZE = 100;
 
 export default function App() {
+  const ref1 = useRef<View>(null);
+  const ref2 = useRef<View>(null);
+  const ref3 = useRef<View>(null);
+  const ref4 = useRef<View>(null);
+
+  const measure = (index: number, ref: RefObject<View>) => {
+    ref.current?.measure((x, y, width, height, pageX, pageY) =>
+      console.log(`${Platform.OS} - ${index + 1} - `, {
+        x,
+        y,
+        width,
+        height,
+        pageX,
+        pageY,
+      })
+    );
+  };
+
+  useEffect(() => {
+    const refs = [ref1, ref2, ref3, ref4];
+
+    setTimeout(() => refs.forEach((ref, index) => measure(index, ref)), 1000);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      {/* WORKS */}
+      <View
+        ref={ref1}
+        style={{
+          width: ITEM_SIZE,
+          height: ITEM_SIZE,
+          backgroundColor: "red",
+          marginLeft: width - 1,
+        }}
+      />
+      {/* WORKS */}
+      <View
+        ref={ref2}
+        style={{
+          width: ITEM_SIZE,
+          height: ITEM_SIZE,
+          marginTop: 50,
+          backgroundColor: "blue",
+          marginLeft: width,
+        }}
+      />
+      {/* WORKS */}
+      <FlatList
+        data={[1]}
+        horizontal
+        keyExtractor={(item) => String(item)}
+        contentContainerStyle={{ paddingLeft: width - 1 }}
+        renderItem={({ item }) => (
+          <View
+            ref={ref3}
+            style={{
+              width: ITEM_SIZE,
+              height: ITEM_SIZE,
+              backgroundColor: "green",
+            }}
+          />
+        )}
+      />
+      {/* DOESN'T WORK */}
+      <FlatList
+        data={[1]}
+        horizontal
+        keyExtractor={(item) => String(item)}
+        contentContainerStyle={{ paddingLeft: width }}
+        renderItem={({ item }) => (
+          <View
+            ref={ref4}
+            style={{
+              width: ITEM_SIZE,
+              height: ITEM_SIZE,
+              backgroundColor: "orange",
+            }}
+          />
+        )}
+      />
     </View>
   );
 }
@@ -13,8 +103,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: 100,
   },
 });
